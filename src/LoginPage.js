1863e35cef0ac98477300955a97e4a96eeda8bad
magiccane204 <134020332+magiccane204@users.noreply.github.com>
2026-03-23 23:48:49 +0530

    Update LoginPage.js

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API = "https://mernwebmanga.onrender.com";
+const API = "https://mernwebmanga.vercel.app/";
 
 function LoginPage({ setMode, setLoggedIn, setUserRole, setUserEmail }) {
 
